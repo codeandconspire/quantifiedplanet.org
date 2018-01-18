233d2a79f7aed9d05d39a4108f98e635cd721d38
Carl Törnqvist <calle.tornqvist@gmail.com>
2018-01-18 14:36:28 +0100

    Add in page navigation

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ if (process.env.NODE_ENV === 'development') {
   app.use(require('choo-devtools')())
 }
 
+// expose reverse route pattern on state (document -> route)
+app.state.routes = {
+  homepage: '/',
+  page: '/:uid',
+  section: '/:uid/:section'
+}
+
 app.route('/', require('./lib/views/home'))
 app.route('/:page', require('./lib/views/page'))
+app.route('/:page/:section', require('./lib/views/page'))
 app.use(require('./lib/stores/api'))
 app.use(require('./lib/stores/pages'))
 app.use(require('./lib/stores/error'))
